14463bf8d4cf838b0a29d9f144799dfd7c7773a7
SujoyKumar108 <sujoykumar108times@gmail.com>
2026-04-05 08:33:51 +0600

    Form Validation

---
+++ jsScript.js
@@ -0,0 +1,76 @@
+document.getElementById("myForm").addEventListener("submit", function(event) {
+
+    event.preventDefault();
+    let username = document.getElementById("username").value.trim();
+    let password = document.getElementById("password").value;
+    let email = document.getElementById("email").value;
+    let dob = document.getElementById("dob").value;
+    let id = document.getElementById("id").value;
+    let dept = document.getElementById("dept").value;
+    let address = document.getElementById("address").value.trim();
+
+    let gender = document.getElementsByName("gender");
+
+    // Username
+    if (username === "") {
+        alert("Username is required");
+        // event.preventDefault();
+        return;
+    }
+
+    // Password
+    if (password.length < 6) {
+        alert("Password must be at least 6 characters");
+        // event.preventDefault();
+        return;
+    }
+
+    // Email
+    if (!email.includes("@")) {
+        alert("Enter valid email");
+        // event.preventDefault();
+        return;
+    }
+
+    // DOB
+    if (dob === "") {
+        alert("Select date of birth");
+        // event.preventDefault();
+        return;
+    }
+
+    // ID
+    if (id === "" || id <= 0) {
+        alert("Enter valid ID");
+        // event.preventDefault();
+        return;
+    }
+
+    // Gender
+    let selected = false;
+    for (let i = 0; i < gender.length; i++) {
+        if (gender[i].checked) {
+            selected = true;
+        }
+    }
+    if (!selected) {
+        alert("Select gender");
+        // event.preventDefault();
+        return;
+    }
+
+    // Department
+    if (dept === "") {
+        alert("Select department");
+        
+        return;
+    }
+
+    // Address
+    if (address === "") {
+        alert("Address is required");
+        // event.preventDefault();
+        return;
+    }
+
+});
