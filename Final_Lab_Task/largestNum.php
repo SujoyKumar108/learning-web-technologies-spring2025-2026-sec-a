36e1a776c5d49f3a3f545146293d7aecf119be8e
SujoyKumar108 <sujoykumar108times@gmail.com>
2026-04-08 23:47:02 +0600

    4th task uploaded

---
+++ Final_Lab_Task/largestNum.php
@@ -0,0 +1,19 @@
+<?php
+$a = 9;
+$b = 8;
+$c = 7;
+
+echo "The numbers are : {$a}, {$b}, {$c} <br>";
+if($a >= $b && $a >= $c)
+    {
+        echo "{$a} is the largest number.";
+    }
+else if($b >= $a && $b >= $c)
+{
+    echo "{$b} is the largest number.";
+}
+else if($c >= $a && $c >= $b)
+    {
+        echo "{$c} is the largest number.";
+    }
+?>
